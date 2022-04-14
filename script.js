3a091720b7721d118639da645f7087eb811ce858
Rebecca Jiang <hanyujia@andrew.cmu.edu>
2022-04-14 10:42:30 -0400

    week 10

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-$("a").click( function() {
-    window.open('www.google.com', '_blank'); } );
-
+function validate(){
+    var txtDob = $("#txtDate").val();
+    var dob = new Date(txtDob);
+    if(dob >= new Date()){
+        alert("Date of Birth is not valid. Date has to be before today.");
+        return false;
+    }
+    var phone = $("#phone").val();
+    if(phone.length != 10 || isNaN(phone)){
+        alert("Phone number is not valid – needs 10 digits or not in a valid format – no characters or hyphens.");
+        return false;
+    }
+}
